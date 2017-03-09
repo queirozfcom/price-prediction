0632d553b4f2dce05955f2db40d216be9ac695bd
queirozfcom <falmeida1988@gmail.com>
2017-03-09 03:33:04 -0300

    new commit (automatic message)

--- src/lib/dataset.py
+++ src/lib/dataset.py
@@ -1,1 +1,15 @@
-dataset.py
+import pandas as pd
+import numpy as np
+
+   
+def make_Xy_simple(dataframe):
+    
+    X = []
+    y = []
+
+    for nparr in dataframe.values:
+        X.append(nparr[:-1].tolist())
+        y.append(nparr[-1])
+
+    return np.array(X),np.array(y)    
+
